641b6cb53cd306bd427f91ae97dfc932ee12aed2
Dan Gorman <danielhgorman@gmail.com>
2016-07-20 09:31:11 -0400

    Done.

--- js/donut-counter.js
+++ js/donut-counter.js
@@ -7,3 +7,32 @@
 //write a conditional to check if there are enough donuts
 
   //alert the user with a message telling them the numbers of donuts and guests, and if there are enough donuts for each guest.
+
+
+var guests 
+undefined
+prompt("Please enter the desired number of donuts")
+"100"
+parseInt(prompt("Please enter the desired number of donuts")
+VM443:1 Uncaught SyntaxError: missing ) after argument list
+parseInt(prompt("Please enter the desired number of donuts"))
+100
+var donuts 
+undefined
+donuts = parseInt(prompt("Please enter the desired number of donuts"))
+50
+donuts
+50
+guests = parseInt(prompt("Please enter the number of guests"))
+30
+if (donuts > guests) {
+VM859:1 Uncaught SyntaxError: Unexpected end of input
+if (donuts > guests) {console.log("not enough fucking donuts bro")}
+VM958:1 not enough fucking donuts bro
+undefined
+if (donuts < guests) {console.log("not enough fucking donuts bro")}
+undefined
+guests
+30
+donuts
+50
